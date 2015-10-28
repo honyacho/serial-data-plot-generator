7fcbd7793871d2b77828a1fb6ca1a35fd275c70f
honyacho <cc_called_pizza@yahoo.co.jp>
2015-10-28 16:28:02 +0900

    extract parameters

--- serial_plot_gen.scala
+++ serial_plot_gen.scala
@@ -2,17 +2,20 @@ import java.io.PrintWriter
 
 val plotFileName = "script.plt"
 val plotTitle = "E_y"
+val xRange: (String, String) = ("1", "577")
+val yRange: (String, String) = ("-1.0e+12", "1.0e+12")
+val imageSize: (Int, Int) = (800, 600)
 
 def fileTemplate(arg: String) = s"""
-set terminal pngcairo enhanced size 800,600 font "Arial,24"
+set terminal pngcairo enhanced size ${imageSize._1},${imageSize._2} font "Arial,24"
 ${arg}
 set terminal wxt
 set output
 """
 
 def plotTemplate(inputFileName: String, outputFileName: String) = s"""
-set xrange [1:577]
-set yrange [-1.0e+12:1.0e+12]
+set xrange [${xRange._1}:${xRange._2}]
+set yrange [${yRange._1}:${yRange._2}]
 set output "${outputFileName}"
 plot "${inputFileName}" using 96 with lines title "${plotTitle}" lw 2 lc rgb "red"
 """
