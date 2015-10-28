7dfaa742a186d1ad24aa53d4ee794f7edddd4077
honyacho <cc_called_pizza@yahoo.co.jp>
2015-10-28 18:27:16 +0900

    modify tics setting

--- serial_plot_gen.scala
+++ serial_plot_gen.scala
@@ -5,8 +5,7 @@ val plotTitle = "E_y"
 val xRange: (String, String) = ("0", "576")
 val yRange: (String, String) = ("-1.0e+12", "1.0e+12")
 val imageSize: (Int, Int) = (800, 600)
-val xtics: List[(String, Int)] = List("0" -> 0, "30.0" -> 186)
-val ytics: List[(String, Int)] = List("0.0" -> 0, "30.0" -> 185, "60.0" -> 370, "90.0" -> 555)
+val xtics: List[(String, Int)] = List("0.0" -> 0, "30.0" -> 185, "60.0" -> 370, "90.0" -> 555)
 
 def fileTemplate(arg: String) = s"""
 set terminal pngcairo enhanced size ${imageSize._1},${imageSize._2} font "Arial,24"
@@ -19,7 +18,6 @@ def plotTemplate(inputFileName: String, outputFileName: String) = s"""
 set xrange [${xRange._1}:${xRange._2}]
 set yrange [${yRange._1}:${yRange._2}]
 set xtics (${xtics.map(t => "\"" + t._1 + "\" " + t._2).mkString(",")})
-set ytics (${ytics.map(t => "\"" + t._1 + "\" " + t._2).mkString(",")})
 set output "${outputFileName}"
 plot "${inputFileName}" using 96 with lines title "${plotTitle}" lw 2 lc rgb "red"
 """
